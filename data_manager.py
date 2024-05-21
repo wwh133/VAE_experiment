0e1355cb8db374c4c74438e84697d082cff023d4
hpjang <31857360+hpjang@users.noreply.github.com>
2024-05-21 10:34:18 +0900

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -4,7 +4,7 @@ import torch
 ########################
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#device = torch.device('cpu')
+#device = torch.device('cpu') # train use cpu
 
 def split_train_dev(datadict, train_percent=0.8):
     train_dict = dict()
